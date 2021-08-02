e3a84147f78cd7b62ff956b708818cb706d839ff
Léo SANCHEZ <leosa@hotmail.fr>
2021-08-02 14:09:11 +0200

    add cv

--- server.py
+++ server.py
@@ -5,15 +5,21 @@ import socketserver
 from webcam import webcam
 from threading import *
 from datetime import datetime
+from peopleDetection import peopleDetection
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+	content = ''
 	def do_GET(self):
 		if ".jpg" in self.path or ".png" in self.path or ".js" in self.path or ".css" in self.path:
 			return http.server.SimpleHTTPRequestHandler.do_GET(self)
-		html = 'Hey you, the Home-IntelligenceV2 is running.</br>'
+		#Auto reload every 5 sec
+		#html = '<meta http-equiv="refresh" content="5">'
+		# Actual content
+		html = 'Hey <b>you</b>, the <i>Home-IntelligenceV2</i> is running.</br>'
 		date = datetime.now()
 		html = html + 'It is ' + str(date) + '</br>'
 		html = html + 'Webcam sample:</br><img src="camCapture.jpg"></img>'
+		html = html + self.content
 		self.send_response(200)
 		self.send_header("Content-type", "text/html")
 		self.end_headers()
@@ -36,10 +42,12 @@ class server:
 		thread.start()
 		
 		userWebcam = webcam()
+		peopleDetec = peopleDetection()
 		i = datetime.now()
-		while 0 == 0:
+		while 0 == 0: # Main loop
 			if (datetime.now() - i).total_seconds() >= 5: # Capture webcam every 5 seconds
 				userWebcam.capture()
+				self.handler.content = '</br>' + str(peopleDetec.computeHowManyPerson()) + ' people detected in capture.'
 				i = datetime.now()
 			
 	def serve_http(self, httpd):
--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 # Author: @leosncz
-from peopleDetection import *
+from imageai.Detection import ObjectDetection
+detector = ObjectDetection()
 
-test = peopleDetection()
+model_path = "yolo-tiny.h5"
+input_path = "camCapture.jpg"
+output_path = "output.jpg"
 
-test.computeHowManyPerson()
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath(model_path)
+detector.loadModel()
+
+detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+
+for eachItem in detection:
+    print(eachItem["name"] , " : ", eachItem["percentage_probability"])
